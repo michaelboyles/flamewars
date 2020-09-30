564c7fb9de15973288bca6bc9d215bb95a164e4e
Michael Boyles <michaelboyles@hotmail.co.uk>
2020-10-01 00:24:08 +0100

    Generate TS types from JSON schemas

---
+++ js/generate-types.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const js2ts = require('json-schema-to-typescript');
+
+const dir = '../schemas/';
+const extension = '.schema.json';
+fs.readdir(dir, (err, files) =>
+    files.forEach(file =>
+        js2ts.compileFromFile(dir + file)
+            .then(ts => fs.writeFileSync('dist/' + file.replace(extension, '') + '.ts', ts))
+    )
+);
