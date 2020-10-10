c05ff6b87c2cd6ebeddf6ec5cb2f16850cf79dee
Michael Boyles <michaelboyles@hotmail.co.uk>
2020-10-10 17:16:27 +0100

    Transform dynamo response to match json schema

--- js/server/get-comments.ts
+++ js/server/get-comments.ts
@@ -1,10 +1,29 @@
 import type { ApiGatewayRequest, ApiGatewayResponse } from './aws';
 import * as AWS from 'aws-sdk';
 import type { Handler } from 'aws-lambda'
+import type { GetAllCommentsResponse, Comment } from '../dist/get-all-comments-response'
+import { ScanOutput } from 'aws-sdk/clients/dynamodb';
 
 AWS.config.update({region: 'eu-west-2'});
 
 const dynamo = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+function convertDataToResponse(data: ScanOutput) {
+    let comments: Comment[] = [];
+
+    data.Items.forEach(item => comments.push({
+        id: item.id.S,
+        author: {
+            name: 'Michael'
+        },
+        text: item.comment.S,
+        timestamp: item.timestamp.S,
+        replies: []
+    }));
+
+    const body: GetAllCommentsResponse = {comments: comments}
+    return body;
+}
 
 export const handler: Handler = function(event: ApiGatewayRequest, context) {
     const url = event.queryStringParameters.url;
@@ -30,7 +49,7 @@ export const handler: Handler = function(event: ApiGatewayRequest, context) {
             else {
                 const response: ApiGatewayResponse = {
                     statusCode: 200,
-                    body: JSON.stringify(data)
+                    body: JSON.stringify(convertDataToResponse(data))
                 };
                 resolve(response);
             }
