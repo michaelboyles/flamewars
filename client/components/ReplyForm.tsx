aaa60c7bc0096c95cca514626f556b93325a7962
Michael Boyles <michaelboyles@hotmail.co.uk>
2020-10-19 01:36:09 +0100

    Client-side message length validation. Server side will be later

--- client/components/ReplyForm.tsx
+++ client/components/ReplyForm.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { LoadingSpinner } from './LoadingSpinner'
 import type { Comment, CommentId } from '../../dist/comment'
 import type { Authorization, PostCommentRequest } from '../../dist/post-comment-request'
-import { AWS_SUBMIT_URL } from '../../config';
+import { AWS_SUBMIT_URL, MAX_COMMENT_LENGTH } from '../../config';
 
 type CommentConsumer = (comment: Comment) => void;
 
@@ -21,6 +21,16 @@ function submitComment(text: string, authorization: Authorization, afterSubmit: 
         .then(r => r.json())
         .then(json => afterSubmit(json.comment))
         .catch(e => console.error(e))
+}
+
+const CommentLengthMessage = (props: {length: number}) => {
+    const charsRemaining = MAX_COMMENT_LENGTH - props.length;
+    const noun = Math.abs(charsRemaining) === 1 ? 'character' : 'characters';
+    return (
+        <span>
+            { charsRemaining < 0 ? `Too long by ${Math.abs(charsRemaining)} ${noun}` : `${charsRemaining} ${noun} remaining`}
+        </span>
+    )
 }
 
 export const ReplyForm = (props: {authorization: Authorization, afterSubmit: CommentConsumer, inReplyTo?: CommentId}) => {
@@ -41,7 +51,8 @@ export const ReplyForm = (props: {authorization: Authorization, afterSubmit: Com
         <form onSubmit={onSubmit}>
             {isSubmitting ? <LoadingSpinner /> : null}
             <textarea onChange={e => setText(e.target.value)} readOnly={isSubmitting} ></textarea>
-            <button type="submit" disabled={isSubmitting}>Post</button>
+            <CommentLengthMessage length={text.length} />
+            <button type="submit" disabled={isSubmitting || text.length > MAX_COMMENT_LENGTH}>Post</button>
         </form>
     );
 }
--- config.ts
+++ config.ts
@@ -1,3 +1,4 @@
 export const GOOGLE_CLIENT_ID = '164705233134-movagpgcoeepgc24qksgil15k2qpde8e.apps.googleusercontent.com'
 export const AWS_GET_URL = 'https://4y01mp2xdb.execute-api.eu-west-2.amazonaws.com/default/comments'
 export const AWS_SUBMIT_URL = 'https://4y01mp2xdb.execute-api.eu-west-2.amazonaws.com/default/post-comment-request-js'
+export const MAX_COMMENT_LENGTH = 2000
