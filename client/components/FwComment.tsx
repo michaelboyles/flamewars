b14b619797822519034a68c55d8689ad214b46fa
Michael Boyles <michaelboyles@hotmail.co.uk>
2020-10-27 23:43:33 +0000

    Added markdown support, #5

--- client/components/FwComment.tsx
+++ client/components/FwComment.tsx
@@ -5,9 +5,17 @@ import type { Comment } from '../../dist/get-all-comments-response'
 import { AWS_GET_URL, DELETED_MESSAGE } from '../../config';
 import { formatPastDate } from '../time';
 import { ReplyForm } from './ReplyForm';
+import ReactMarkdown = require('react-markdown');
 
 function isOwner(authorization: LocalAuthorization, comment: Comment) {
     return authorization && comment.author.id.endsWith(authorization.id);
+}
+
+function addLineBreaks(comment: string) {
+    // Replace any newline that is not preceeded or followed by another new line (since if there are 2, they will become a <p>), and *is* followed by a 
+    // word character (because some markdown such as --- on the following line indicates a title, and we can leave that as-is), with a literal slash and
+    // a newline. This will make ReactMarkdown add a <br>
+    return comment.replace(/(?<!\n)\n(?!\n)(?=\w)/g, '\\\n')
 }
 
 export const FwComment = (props: {comment: Comment, authorization: LocalAuthorization}) => {
@@ -23,7 +31,7 @@ export const FwComment = (props: {comment: Comment, authorization: LocalAuthoriz
 
     if (isDeleted && !replies.length) return null;
 
-    const text = isDeleted ? DELETED_MESSAGE : props.comment.text;
+    const text = isDeleted ? DELETED_MESSAGE : addLineBreaks(props.comment.text);
     return (
         <li className='comment'>
             <img className='portrait' src={props.comment.author.portraitUrl ? props.comment.author.portraitUrl : 'https://via.placeholder.com/100x100' } />
@@ -31,7 +39,7 @@ export const FwComment = (props: {comment: Comment, authorization: LocalAuthoriz
                 <span className='author-name'>{props.comment.author.name}</span>
                 <span className='timestamp'>{formatPastDate(Date.parse(props.comment.timestamp))}</span>
                 { isOwner(props.authorization, props.comment) ?<a className='delete-btn' onClick={() => deleteComment()}>Delete</a> : null }
-                <span className='content'>{text}</span>
+                <ReactMarkdown className='content'>{text}</ReactMarkdown>
                 <a onClick={() => setReplyOpen(!isReplyOpen)} className={'reply-btn ' + (isReplyOpen ? 'open' : 'closed')}>Reply</a>
                 {
                     isReplyOpen ? <ReplyForm authorization={props.authorization}
