071817b8e9b949d32b2a697130dd0b30451bac16
Michael Boyles <michaelboyles@hotmail.co.uk>
2020-11-02 23:15:49 +0000

    Added json schema for comment count response

--- client/comment-count.ts
+++ client/comment-count.ts
@@ -1,5 +1,6 @@
 const COMMENT_ANCHOR = '#comments';
 const COUNT_URL = 'https://4y01mp2xdb.execute-api.eu-west-2.amazonaws.com/default/comment-count';
+import { GetCommentCountResponse } from  '../dist/comment-count'
 import { normalizeUrl } from '../common/util'
 
 function getFlamewarsLinks(): HTMLAnchorElement[] {
@@ -20,12 +21,12 @@ function applyCountToCommentLinks() {
 
     fetch(COUNT_URL + queryString)
         .then(resp => resp.json())
-        .then(json => {
-            for (const url in json) {
-                const matchingLink = links.find(link => normalizeUrl(link.href) === url);
-                if (matchingLink) matchingLink.text = json[url] + ' Comments';
-            }
-        }); 
+        .then((json: GetCommentCountResponse) => 
+            json.counts.forEach(urlAndCount => {
+                const matchingLink = links.find(link => normalizeUrl(link.href) === urlAndCount.url);
+                if (matchingLink) matchingLink.text = urlAndCount.count + ' Comments';
+            })
+        );
 }
 
 export default applyCountToCommentLinks;
--- server/get-number-of-comments.ts
+++ server/get-number-of-comments.ts
@@ -2,15 +2,11 @@ import { ApiGatewayRequest, ApiGatewayResponse, getDynamoDb } from './aws';
 import type { Handler } from 'aws-lambda'
 import { QueryInput } from 'aws-sdk/clients/dynamodb';
 import { CORS_HEADERS } from './common';
+import { GetCommentCountResponse, CommentCount } from '../dist/comment-count'
 
 const dynamo = getDynamoDb();
 
-type UrlAndCount = {
-    url: string;
-    count: number;
-}
-
-function queryForUrl(url: string): Promise<UrlAndCount> {
+function queryForUrl(url: string): Promise<CommentCount> {
     const params: QueryInput = {
         TableName: 'FLAMEWARS',
         KeyConditionExpression: "PK = :u", 
@@ -39,8 +35,10 @@ export const handler: Handler = async function(event: ApiGatewayRequest, _contex
     const uniqueUrls = Array.from(new Set(urls.split(',')));
     const results = await Promise.all(uniqueUrls.map(queryForUrl));
 
-    const body = {}; //TODO add a schema
-    results.forEach(result => body[result.url] = result.count);
+    const body: GetCommentCountResponse = {
+        success: true,
+        counts: results 
+    };
 
     const response: ApiGatewayResponse = {
         statusCode: 200,
