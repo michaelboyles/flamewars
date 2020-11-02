bf51277023b6a88ee75e145d076c7e0180355668
Michael Boyles <michaelboyles@hotmail.co.uk>
2020-11-02 23:26:52 +0000

    Limit number of URLs per comment count request

--- server/get-number-of-comments.ts
+++ server/get-number-of-comments.ts
@@ -4,7 +4,30 @@ import { QueryInput } from 'aws-sdk/clients/dynamodb';
 import { CORS_HEADERS } from './common';
 import { GetCommentCountResponse, CommentCount } from '../dist/comment-count'
 
+const MAX_URLS = 30;
 const dynamo = getDynamoDb();
+
+function isRequestValid(request: ApiGatewayRequest) {
+    return getUniqueUrls(request).length <= MAX_URLS;
+}
+
+function getUniqueUrls(request: ApiGatewayRequest): string[] {
+    const urls: string = request.queryStringParameters.urls;
+    return Array.from(new Set(urls.split(',')));
+}
+
+function failResponse(): ApiGatewayResponse {
+    const failBody: GetCommentCountResponse = {
+        success: false,
+        counts: [] 
+    };
+    const failResponse: ApiGatewayResponse = {
+        statusCode: 400,
+        headers: CORS_HEADERS,
+        body: JSON.stringify(failBody)
+    }
+    return failResponse;
+}
 
 function queryForUrl(url: string): Promise<CommentCount> {
     const params: QueryInput = {
@@ -30,11 +53,10 @@ function queryForUrl(url: string): Promise<CommentCount> {
     });
 }
 
-export const handler: Handler = async function(event: ApiGatewayRequest, _context) {
-    const urls: string = event.queryStringParameters.urls;
-    const uniqueUrls = Array.from(new Set(urls.split(',')));
-    const results = await Promise.all(uniqueUrls.map(queryForUrl));
+export const handler: Handler = async function(request: ApiGatewayRequest, _context): Promise<ApiGatewayResponse> {
+    if (!isRequestValid(request)) return failResponse();
 
+    const results = await Promise.all(getUniqueUrls(request).map(queryForUrl));
     const body: GetCommentCountResponse = {
         success: true,
         counts: results 
