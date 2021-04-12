67c0f07a0c391fc39f857f324aa8743962c14b98
Michael Boyles <michaelboyles@hotmail.co.uk>
2021-04-12 21:31:16 +0100

    #3 parameterize CORS URL

--- server/common.ts
+++ server/common.ts
@@ -1,4 +1,4 @@
 export const CORS_HEADERS = {
-    'Access-Control-Allow-Origin': 'http://localhost:8080',
+    'Access-Control-Allow-Origin': process.env.CORS_ALLOW_ORIGIN,
     'Access-Control-Allow-Methods': 'GET,POST,DELETE,PATCH'
 };
