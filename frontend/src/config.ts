4768c591c534f8894e58a26fa3bd5d426b99981b
Michael Boyles <michael@boyl.es>
2021-12-24 22:55:24 +0000

    chore: add docs for config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,10 +1,35 @@
 export type Config = {
+    /**
+     * The AWS API URL which was automatically generated as part of CloudFormation deployment. Mandatory.
+     */
     awsUrl: string;
-    allowImages?: boolean;
-    deletedMessage?: string;
+    /**
+     * The Google client ID to use for authorization. Either this must be present, or specified via a meta tag:
+     * https://developers.google.com/identity/sign-in/web/sign-in#specify_your_apps_client_id. 
+     * This takes precedence over the meta tag.
+     */
     googleClientId?: string;
+
+    /**
+     * The ID of the HTML element which comments should be inserted into. Default: "comments"
+     */
     htmlContainerId?: string;
+    /**
+     * Whether to allow images inserted into comments (involves hotlinking to other domains). Default: false
+     */
+    allowImages?: boolean;
+    /**
+     * The message to display when a comment has been deleted. Default: "Comment was deleted" 
+     */
+    deletedMessage?: string;
+    /**
+     * Whether to use infinite scroll to load more comments when comments are paginated. If false, there's
+     * a button to load more. Default: true
+     */
     useInfiniteScroll?: boolean;
+    /**
+     * Whether to include a link to FlameWars' GitHub repo. Default: true
+     */
     displayGitHubLink?: boolean;
 };
 
